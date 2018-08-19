a5765aba8d612d434212144b425778d932503a71
Jony Santos <jonyferreira@hotmail.com>
2018-08-19 15:03:41 -0300

    Initial commit

---
+++ src/main/java/br/com/jonyfs/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package br.com.jonyfs.config;
+
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @PostConstruct
+    public void logMessage() {
+        LOGGER.info("STARTED");
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
