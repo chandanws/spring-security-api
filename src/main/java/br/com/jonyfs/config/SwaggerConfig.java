aeed9dca1c5ac7a2c8860692341f3d51d65b01da
Jony Santos <jonyferreira@hotmail.com>
2018-08-21 21:36:49 -0300

    initial commit

--- src/main/java/br/com/jonyfs/config/SwaggerConfig.java
+++ src/main/java/br/com/jonyfs/config/SwaggerConfig.java
@@ -4,16 +4,14 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Import(SpringDataRestConfiguration.class)
+//@Import(SpringDataRestConfiguration.class) TODO WAIT FOR SPRING BOOT 2.0 COMPATIBILITY
 @EnableSwagger2
 @Slf4j
 public class SwaggerConfig {
