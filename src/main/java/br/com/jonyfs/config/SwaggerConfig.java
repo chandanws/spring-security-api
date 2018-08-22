9e1b8ecfc6ed722c299b8fb6f3080101fa5f272e
Jony Santos <jonyferreira@hotmail.com>
2018-08-21 21:25:04 -0300

    initial commit

--- src/main/java/br/com/jonyfs/config/SwaggerConfig.java
+++ src/main/java/br/com/jonyfs/config/SwaggerConfig.java
@@ -4,13 +4,16 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@Import(SpringDataRestConfiguration.class)
 @EnableSwagger2
 @Slf4j
 public class SwaggerConfig {
@@ -19,7 +22,6 @@ public class SwaggerConfig {
     public void logMessage() {
         LOGGER.info("STARTED");
     }
-
 
     @Bean
     public Docket api() {
--- src/main/java/br/com/jonyfs/user/NewUserController.java
+++ src/main/java/br/com/jonyfs/user/NewUserController.java
@@ -7,16 +7,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@RequestMapping("/newUser")
 public class NewUserController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping(name = "/newUser", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> registerNewUser(@Valid @RequestBody UserDto userDto) {
         User savedUser = userService.registerNewUserAccount(userDto);
 
